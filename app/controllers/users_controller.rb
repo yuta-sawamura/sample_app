a84d26fb7363f2095751aea59d6c96ca90da1efc
Yuuta Sawamura <sawamura1009@gmail.com>
2021-05-25 13:05:52 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -53,14 +54,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
     
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      store_location
-      unless logged_in?
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    # beforeフィルター
     
     # 正しいユーザーかどうか確認
     def correct_user
